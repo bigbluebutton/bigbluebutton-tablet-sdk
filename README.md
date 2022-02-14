afbeb80dd03934cd1cbb56ac62c573fab623ceb4
Tiago Jacobs <tiago.jacobs@gmail.com>
2022-02-14 01:20:50 -0300

    Initialize pod library

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # bigbluebutton-mobile-lib
+
+## IOS
+
+In order to open the workspace to build this library, you can run:
+```sh
+open ios/BigBlueButtonMobileSDK/Example/BigBlueButtonMobileSDK.xcworkspace
+```
---
+++ ios/BigBlueButtonMobileSDK/Example/BigBlueButtonMobileSDK/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BigBlueButtonMobileSDK
+//
+//  Created by Tiago Jacobs on 02/14/2022.
+//  Copyright (c) 2022 Tiago Jacobs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
