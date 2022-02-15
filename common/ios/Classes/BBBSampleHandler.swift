520101d2f6add42c763472c26eaafacf5590dada
Tiago Jacobs <tiago.jacobs@gmail.com>
2022-02-15 18:41:42 -0300

    Add class BBBSampleHandler to be used by example project (keep logic in SDK)

---
+++ common/ios/Classes/BBBSampleHandler.swift
@@ -0,0 +1,40 @@
+import ReplayKit
+import os
+
+open class BBBSampleHandler : RPBroadcastSampleHandler {
+    // Logger (these messages are displayed in the console application)
+    private var logger = os.Logger(subsystem: "BigBlueButtonMobileSDK", category: "BBBSampleHandler")
+    
+    open override func broadcastStarted(withSetupInfo setupInfo: [String : NSObject]?) {
+        logger.info("ReplayKit2 event - broadcastStarted")
+    }
+    
+    open override func broadcastPaused() {
+        logger.info("ReplayKit2 event - broadcastPaused")
+    }
+    
+    open override func broadcastResumed() {
+        logger.info("ReplayKit2 event - broadcastResumed")
+    }
+    
+    open override func broadcastFinished() {
+        logger.info("ReplayKit2 event - broadcastFinished")
+    }
+    
+    open override func processSampleBuffer(_ sampleBuffer: CMSampleBuffer, with sampleBufferType: RPSampleBufferType) {
+        switch sampleBufferType {
+        case RPSampleBufferType.video:
+            logger.trace("ReplayKit2 event - processSampleBuffer(video)")
+            break
+        case RPSampleBufferType.audioApp:
+            logger.trace("ReplayKit2 event - processSampleBuffer(audioApp)")
+            break
+        case RPSampleBufferType.audioMic:
+            logger.trace("ReplayKit2 event - processSampleBuffer(audioMic)")
+            break
+        @unknown default:
+            // Handle other sample buffer types
+            fatalError("Unknown type of sample buffer")
+        }
+    }
+}
